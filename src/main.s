b07457d649bdae00f32424106a1078ddac664068
Jan Waltl <waltl.jan@gmail.com>
2023-03-12 21:21:22 +0100

    Refactor project
    - Enlarge stack
    - Put vtable IRQs into separate assembly
    - Main function

---
+++ src/main.s
@@ -0,0 +1,14 @@
+.syntax unified
+.thumb
+.eabi_attribute Tag_ABI_align_preserved, 1
+
+// Globally exported symbols
+.global main
+
+.thumb_func
+main:
+	// Add 1 to register 'r0'.
+	ADDS r0, r0, #1
+	// Loop back.
+	B    main
+.size main, .-main
