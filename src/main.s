12f2c1cb1a657843a41356f61d39fe4471e6d94c
Jan Waltl <waltl.jan@gmail.com>
2023-03-12 22:25:13 +0100

    experiment with ARM functions

--- src/main.s
+++ src/main.s
@@ -6,10 +6,28 @@
 // Globally exported symbols
 .global main
 
+
+.thumb_func
+foo:
+	ADDS r1, r1, #1
+	PUSH {LR}
+	BL foo
+	POP {PC}
+.size foo, .-foo
+
+.thumb_func
+bar:
+	ADDS r2, r2, #1
+	BX LR
+.size bar, .-bar
+
 .thumb_func
 main:
-	// Add 1 to register 'r0'.
+	EOR r0, r0
+	EOR r1, r1
+	EOR r2, r2
+loop:
 	ADDS r0, r0, #1
-	// Loop back.
-	B    main
+	BL foo
+	B loop
 .size main, .-main
