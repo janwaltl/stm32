2067a146d21d479e7826bc71540bf77cf40ed3a2
Jan Waltl <waltl.jan@gmail.com>
2023-03-12 21:01:39 +0100

    Proper vtable

--- src/core.s
+++ src/core.s
@@ -6,24 +6,44 @@
 .global vtable
 .global reset_handler
 
+// Set by linker script
 .type _estack, %object
 
-
-/*
- * The actual vector table.
- * Only the size of RAM and 'reset' handler are
- * included, for simplicity.
- */
+// Vector table.
 .type vtable, %object
 vtable:
-    .word _estack
-    .word reset_handler
+    .word _estack 					// Initial stack pointer
+    .word reset_handler 			// Reset handler
+	.word unhandled 				// NMI
+	.word unhandled 				// Hard fault
+	.word unhandled 				// Memory fault
+	.word unhandled 				// Bus fault
+	.word unhandled 				// Usage fault
+	.word 0 						// Reserved
+	.word 0 						// Reserved
+	.word 0 						// Reserved
+	.word 0 						// Reserved
+	.word unhandled 				// SVCall
+	.word 0 						// Reserved
+	.word 0 						// Reserved
+	.word unhandled 				// PendSV
+	.word unhandled 				// Systick
+	. = 0x400 						// IRQs
 .size vtable, .-vtable
+
+
+.thumb_func
+unhandled:
+	LDR  r0, =0xDEADBEEF
+	LDR  r1, =0xDEADBEEF
+	LDR  r2, =0xDEADBEEF
+	LDR  r3, =0xDEADBEEF
+	B unhandled
+.size unhandled, .-unhandled
 
 .thumb_func
 reset_handler:
-	// Set the stack pointer to the end of the stack.
-	// The '_estack' value is defined in our linker script.
+	// Clean stack
 	LDR  r0, =_estack
 	MOV  sp, r0
 
